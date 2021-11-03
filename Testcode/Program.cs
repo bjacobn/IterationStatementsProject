752eb68b07614cb6ad115339c9702fcab7631fc0
Jacob <bjacobcode@gmail.com>
2021-11-03 10:02:34 -0700

    intial commit

---
+++ Testcode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Testcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            int num;
+            num = 0;
+
+            do
+            {
+               
+                num++;
+                numbers.Add(num);
+
+            } while (num < 100);
+
+           
+            while (num < 200)
+            {
+              
+                num++;
+                numbers.Add(num);
+            }
+
+            Console.WriteLine("Increase:");
+
+            foreach (var parts in numbers)
+            {
+                Console.WriteLine(parts);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Decrease:");
+
+
+        }
+    }
+}
